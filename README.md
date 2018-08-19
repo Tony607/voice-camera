156769f7572a4726488cf32b2c2fa62285faf7b8
Tony607 <Tony607@users.noreply.github.com>
2018-08-19 15:23:40 +0800

    modify readme and install package

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Voice activated object detection camera
+# [Voice activated object detection camera](https://www.dlology.com/blog/diy-object-detection-doodle-camera-with-raspberry-pi-part-1/)
 ## Install
 ```
 ./install.sh
@@ -7,4 +7,12 @@
 On Raspberry Pi 3 or 3B run
 ```
 python3 porcupine_voice_activate.py --keywords blueberry,pineapple
+```
+Or just
+```
+./run.sh
+```
+If it says is not runnable.
+```
+chmod +x ./run.sh
 ```
--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo apt update
-sudo apt install libatlas-base-dev protobuf-compiler python-pil python-lxml python-tk libqtgui4 python-opencv
+# Install dependencies for TensorFlow Object detection API
+sudo apt install -y libatlas-base-dev protobuf-compiler python-pil python-lxml python-tk libqtgui4 python-opencv
+# Install dependencies for Porcupine
+sudo apt install -y libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0 ffmpeg libav-tools
 echo "Installing python dependencies"
 sudo pip3 install -r ./requirements.txt
 # TensorFlow object detection dependencies
