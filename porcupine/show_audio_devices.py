f36dc2be7fe93b502d93f2dc46d9363b64355e2f
Tony607 <Tony607@users.noreply.github.com>
2018-08-20 16:39:41 +0800

    show_audio_devices show all devices

--- porcupine/show_audio_devices.py
+++ porcupine/show_audio_devices.py
@@ -5,5 +5,7 @@ for i in range(pa.get_device_count()):
     info = pa.get_device_info_by_index(i)
     if 'usb' in info['name'].lower():
       print('\n\rLocated USB audio input device at index:', info['index'], ', name:', str(info['name']).encode('utf-8'))
+    else:
+      print('\n\rAudio input device at index:', info.get('index'), ', name:', str(info.get('name')).encode('utf-8'))
 
 pa.terminate()
